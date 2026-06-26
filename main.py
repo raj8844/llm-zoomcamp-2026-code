3722c870e40b6d87f5f39c87d47af23f74cfcad0
Rajesh Shelar <raj8844@gmail.com>
2026-06-26 13:59:45 +0000

    Chapter 1: Woking rag with Google API Key using OpenAI

--- main.py
+++ main.py
@@ -5,13 +5,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-from google import genai
-
-
-client = genai.Client(api_key="AQ.Ab8RN6KlIaYdcK89Y5mYaBkM-02iSuRhtQ3-hgMut_qsxxLwKA")
-
-interaction = client.interactions.create(
-    model="gemini-3.5-flash",
-    input="Explain how AI works in a few words"
-)
-print(interaction.output_text)
